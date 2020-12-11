7d3cb98fe471b298ed1459034053078fc2009ef2
rakshitha126 <75771847+rakshitha126@users.noreply.github.com>
2020-12-11 20:17:55 +0530

    Find grade

--- FindGrade.java
+++ FindGrade.java
@@ -1,26 +1,20 @@
-class FindGrade
-{
-	public static void main(String args[])
-	{
-		System.out.println("Finding Grade for percentage");
-		
+
+public class FindGrade {
+
+	public static void main(String[] args) {
+		System.out.println("Find greade for percentage");
 		float percentage = 78.9f;
-		
 		if(percentage >= 85)
 		{
-			System.out.println("You have got A");
+			System.out.println("you have got A");
+		}else if(percentage >= 65 && percentage <= 84) {
+			System.out.println("you have got B");
+		}else if(percentage >= 35 && percentage <= 64) {
+			System.out.println("you have got C");
 		}
-		else if(percentage >= 65 && percentage <= 84)
-		{ 
-			System.out.println("You have got B");
-		}
-		else if(percentage >= 35 && percentage <=64)
-		{
-			System.out.println("You have got C");
-		}
-		else 
-		{
-			System.out.println("You have got D");
-		}
+		else
+			System.out.println("you have got D");
+	
+			
 	}
 }
