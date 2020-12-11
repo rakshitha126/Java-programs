fa65bdd153b5236bf2d1359f20a40c575562b3c9
rakshitha126 <75771847+rakshitha126@users.noreply.github.com>
2020-12-11 19:45:09 +0530

    programs

---
+++ BasicPgm.java
@@ -0,0 +1,13 @@
+
+public class BasicPgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adding 2 numbers");
+		int a=2,b=20,sum;
+		sum=a+b;
+		System.out.println(+sum);
+
+	}
+
+}
---
+++ ForLoop.java
@@ -0,0 +1,17 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5,a;
+		System.out.println("to print for loop");
+		for(a=0;a<5;a++)
+		{
+			System.out.println("a");
+			
+		}
+			
+
+	}
+
+}
