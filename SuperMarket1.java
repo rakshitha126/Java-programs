dc3eb13c6d2800c7063d70afcc92d168185c3b39
rakshitha126 <75771847+rakshitha126@users.noreply.github.com>
2021-06-21 23:06:19 +0530

    Add files via upload

---
+++ SuperMarket1.java
@@ -0,0 +1,66 @@
+class SuperMarket1{
+public static void main(String a[])
+{
+String perfume[]={"fogg","secret","engage","yardley","Wild stone"};
+System.out.println(perfume.length);
+for(int i=0;i<perfume.length;i++)
+{
+	System.out.println(perfume[i]);
+}
+
+String vegetablesAndFruits[]={"strawberry","mango","tomato","potato","grapes"};
+System.out.println(vegetablesAndFruits.length);
+for(int i=0;i<vegetablesAndFruits.length;i++)
+{
+	System.out.println(vegetablesAndFruits[i]);
+}
+String biscuits[]={"hide & seek","gooday","bourbon","moms magic","parleg"};
+System.out.println(biscuits.length);
+for(int i=0;i<biscuits.length;i++)
+{
+	System.out.println(biscuits[i]);
+}
+String cereals[]={"wheat","rice","barley","oats","maize"};
+System.out.println(cereals.length);
+for(int i=0;i<cereals.length;i++)
+{
+	System.out.println(cereals[i]);
+}
+	String chocolate[]={"perk","dairymilk","milkybar","kitkat","munch"};
+System.out.println(chocolate.length);
+for(int i=0;i<chocolate.length;i++)
+{
+	System.out.println(chocolate[i]);
+}
+	String soap[]={"mysore sandal","lux","santoor","margo","lifebuoy"};
+System.out.println(soap.length);
+for(int i=0;i<soap.length;i++)
+{
+	System.out.println(soap[i]);
+}
+String cookingOil[]={"sunflower","goldwinner","coconut oil","olive oil","peanut oil"};
+System.out.println(cookingOil.length);
+for(int i=0;i<cookingOil.length;i++)
+{
+	System.out.println(cookingOil[i]);
+}
+String iceCream[]={"butterscotch","strawberry","vanila","mango","chocolate"};
+System.out.println(iceCream.length);
+for(int i=0;i<iceCream.length;i++)
+{
+	System.out.println(iceCream[i]);
+}
+String accessories[]={"hair oil","comb","wallet","shampoo","nail polish"};
+System.out.println(accessories.length);
+for(int i=0;i<accessories.length;i++)
+{
+System.out.println(accessories[i]);
+}
+String coolDrinks[]={"maaza","sprite","fanta","coke","mirinda"};
+System.out.println(coolDrinks.length);
+for(int i=0;i<coolDrinks.length;i++)
+{
+System.out.println(coolDrinks[i]);
+}
+}
+} 
